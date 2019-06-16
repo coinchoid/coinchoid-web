c44ee8dddfb006eaf648e78186985fae7a7fcba3
nicgirault <nic.girault@gmail.com>
2019-06-16 18:22:50 +0200

    commit build

---
+++ build/precache-manifest.ac2c913364b61df2bf953d412ced9ff1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39411310930fb75c9b71c5bf56ced86d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2827bde00fef81087500",
+    "url": "/static/js/2.b2d8f36b.chunk.js"
+  },
+  {
+    "revision": "ede28caa6e8a06878c32",
+    "url": "/static/js/main.ce1f4332.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
